1121bd8eb168dac287bdbf2ba2befffe5a55da80
Koerismo <56563658+koerismo@users.noreply.github.com>
2024-06-16 17:57:45 -0400

    feat: Lazy game search, update deps

--- src/game.ts
+++ src/game.ts
@@ -20,12 +20,6 @@ function parseSearchPath(sp: string, paths: { game?: string, mod: string, gi: st
 	return normalize(join(paths.game, sp));
 }
 
-// function parse2(path: string, config: { mod: string, cwd: string, game?: string }) {
-// 	return {
-		
-// 	}
-// }
-
 // function matchSearchPath(fs: ReadableFileSystem, sp: string): string[] {
 // 	if (!(fs instanceof NodeSystem)) throw Error(`Glob searching in virtual filesystems is not implemented!`);
 // 	return globSync(sp);
@@ -101,9 +95,10 @@ async function readKV(fs: ReadableFileSystem, path: string): Promise<KeyVRoot|un
 export class SteamCache {
 	public fs: ReadableFileSystem;
 	public root: string;
-	
-	cache: Record<string, string> = {};
-	initialized: InitState = InitState.None;
+
+	applibcache: Record<string, string> = {};
+	appdircache: Record<string, string|undefined> = {};
+	public initialized: InitState = InitState.None;
 	
 	static cachecache: Record<string, SteamCache> = {};
 	public static get(fs: ReadableFileSystem, root: string) {
@@ -118,6 +113,7 @@ export class SteamCache {
 	}
 
 	async parse(): Promise<boolean> {
+		if (this.initialized) return this.initialized === InitState.Ready;
 		this.initialized = InitState.Error;
 
 		// Get the list of libraries
@@ -132,21 +128,8 @@ export class SteamCache {
 			const lib_apps = library.dir('apps').all().map(x => x.key);
 			console.log(`Resolving ${lib_apps.length} apps from library '${lib_path}'`);
 			
-			app: for (const app of lib_apps) {
-				const appmanifest = await readKV(this.fs, join(lib_path, 'steamapps/appmanifest_'+app+'.acf'));
-				if (!appmanifest) continue app;
-				
-				try {
-					const app_root = appmanifest.dir('AppState');
-					const app_dir = app_root.pair('installdir').string();
-					this.cache[app] = join(lib_path, 'steamapps/common', app_dir+'/');
-				}
-				catch(e) {
-					console.error('Failed to parse appmanifest for appid', app, e);
-				} 
-
-				// Lots of info here, but we don't need most of it.
-				// const app_name = app_root.pair('name').string();
+			for (const app of lib_apps) {
+				this.applibcache[app] = lib_path;
 			}
 		}
 
@@ -154,15 +137,49 @@ export class SteamCache {
 		return true;
 	}
 
-	async findGame(appid: string): Promise<string|undefined> {
+	async parseGame(appid: string, force=false): Promise<string|undefined> {
+		const lib_path = this.applibcache[appid];
+		if (!lib_path) return undefined;
+		if (!force && appid in this.appdircache) return undefined;
+	
+		const appmanifest = await readKV(this.fs, join(lib_path, 'steamapps/appmanifest_'+appid+'.acf'));
+		if (!appmanifest) return;
+		
 		try {
-			if (!this.initialized) await this.parse();
-			if (appid in this.cache) return this.cache[appid];
-			return undefined;
+			const app_root = appmanifest.dir('AppState');
+			const app_dir = app_root.pair('installdir').string();
+			this.appdircache[appid] = join(lib_path, 'steamapps/common', app_dir+'/');
 		}
-		catch {
-			return undefined;
+		catch(e) {
+			console.error('Failed to parse appmanifest for appid', appid, e);
+			this.appdircache[appid] = undefined;
 		}
+
+		// Lots of info here, but we don't need most of it.
+		// const app_name = app_root.pair('name').string();
+		return this.appdircache[appid];
+	}
+
+	async findGame(appid: string): Promise<string|undefined> {
+		if (!this.initialized) await this.parse();
+		if (!(appid in this.appdircache)) await this.parseGame(appid);
+		return this.appdircache[appid];
+	}
+
+	async getInstalled(): Promise<string[]> {
+		if (!this.initialized) await this.parse();
+		return Object.keys(this.applibcache);
+	}
+
+	async getGameAssets(appid: string) {
+		return {
+			header: join(this.root, 'appcache/librarycache', `${appid}_header.jpg`),
+			hero: join(this.root, 'appcache/librarycache', `${appid}_library_hero.jpg`),
+			hero_blur: join(this.root, 'appcache/librarycache', `${appid}_library_hero_blur.jpg`),
+			card: join(this.root, 'appcache/librarycache', `${appid}_library_600x900.jpg`),
+			icon: join(this.root, 'appcache/librarycache', `${appid}_icon.jpg`),
+			logo: join(this.root, 'appcache/librarycache', `${appid}_logo.png`),
+		};
 	}
 }
 
@@ -186,12 +203,15 @@ export class GameSystem implements ReadableFileSystem {
 	public name!: string;
 	public fs: ReadableFileSystem;
 	public modroot: string;
+	public appid?: string;
 	public gameroot?: string;
 	public initialized: InitState = InitState.None;
 
 	steam: SteamCache;
 	providers: [string[], VpkSystem|FolderSystem][] = [];
 	mounts: GameSystem[] = [];
+	_vpks: [string[], VpkSystem][] = [];
+	_loose: [string[], FolderSystem][] = [];
 
 	constructor(fs: ReadableFileSystem, root: string, steam?: SteamCache) {
 		this.fs = fs;
@@ -215,6 +235,7 @@ export class GameSystem implements ReadableFileSystem {
 
 		// Set title
 		this.name = gi_root.parent!.pair('game').string();
+		this.appid = gi_appid;
 
 		// Setup directories
 		const dir_gi = this.modroot;
@@ -284,6 +305,8 @@ export class GameSystem implements ReadableFileSystem {
 		}
 
 		this.providers = working;
+		this._vpks = this.providers.filter(x => x[1] instanceof VpkSystem) as [string[], VpkSystem][];
+		this._loose = this.providers.filter(x => x[1] instanceof FolderSystem) as [string[], FolderSystem][];
 		this.initialized = InitState.Ready;
 		return true;
 	}
@@ -299,8 +322,28 @@ export class GameSystem implements ReadableFileSystem {
 		}
 	}
 
-	async readFile(path: string, qualifier?: string): Promise<Uint8Array | undefined> {
+	async readFile(path: string, qualifier?: string, preferVpk: boolean=false): Promise<Uint8Array | undefined> {
 		if (!await this.validate()) return undefined;
+
+		if (preferVpk) {
+			for (const provider of this._vpks) {
+				if (qualifier && !provider[0].includes(qualifier)) continue;
+				const file = await provider[1].readFile(path);
+
+				if (file === undefined) continue;
+				return file;
+			}
+
+			for (const provider of this._loose) {
+				if (qualifier && !provider[0].includes(qualifier)) continue;
+	
+				const file = await provider[1].readFile(path);
+				if (file === undefined) continue;
+				return file;
+			}
+
+			return undefined;
+		}
 
 		for (const provider of this.providers) {
 			if (qualifier && !provider[0].includes(qualifier)) continue;
@@ -313,8 +356,28 @@ export class GameSystem implements ReadableFileSystem {
 		return undefined;
 	}
 
-	async getPath(path: string, qualifier?: string): Promise<string|undefined> {
+	async getPath(path: string, qualifier?: string, preferVpk: boolean=false): Promise<string|undefined> {
 		if (!await this.validate()) return undefined;
+
+		if (preferVpk) {
+			for (const provider of this._vpks) {
+				if (qualifier && !provider[0].includes(qualifier)) continue;
+				
+				const file = await provider[1].stat(path);
+				if (file === undefined) continue;
+				return provider[1].getPath(path);
+			}
+			
+			for (const provider of this._loose) {
+				if (qualifier && !provider[0].includes(qualifier)) continue;
+				
+				const file = await provider[1].stat(path);
+				if (file === undefined) continue;
+				return provider[1].getPath(path);
+			}
+
+			return undefined;
+		}
 
 		for (const provider of this.providers) {
 			if (qualifier && !provider[0].includes(qualifier)) continue;
